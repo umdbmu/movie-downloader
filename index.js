800f8adef8916d7bec6c219a54e8569d55db0772
umdbmu <umdlbmu@gmail.com>
2020-05-04 17:56:29 +0900

    BotKitの4以降に対応

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 'use strict'
-const Botkit = require('botkit');
+let {
+    Botkit
+} = require('botkit');
+const {
+    SlackAdapter,
+    SlackMessageTypeMiddleware
+} = require('botbuilder-adapter-slack');
 
-const controller = Botkit.slackbot({
-    debug: false
+const adapter = new SlackAdapter({
+    clientSigningSecret: process.env.ClientSigningSecret,
+    botToken: process.env.BotToken
+});
+adapter.use(new SlackMessageTypeMiddleware());
+
+const controller = new Botkit({
+    adapter: adapter,
 });
 
-controller.spawn({
-    token: process.argv[2],
-    retry: true
-}).startRTM(function (err) {
-    if (err) {
-        throw new Error(err);
-    }
-});
+controller.on('slash_command', function (bot, message) {
 
-controller.hears('<(https?://.*)>', ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
-    let url = message.match[1] + '';
+    bot.replyPublic(message, 'Everyone can see this part of the slash command');
+    bot.replyPrivate(message, 'Only the person who used the slash command can see this.');
+
+    console.log(message);
+    // TOOD: ダウンロード処理の開始
     const execSync = require('child_process').exec;
-    const result = execSync(`youtube-dl ${url} -o '%(id)s.%(ext)s'`,
-        { maxBuffer: 400 * 1024 },
+    const result = execSync(`youtube-dl ${message.text} -o '%(id)s.%(ext)s'`, {
+            maxBuffer: 400 * 1024
+        },
         (error, stdout, stderr) => {
             if (error != null) {
-                bot.reply(message, 'download failed!');
+                bot.replyPrivate(message, 'download failed!');
                 console.log(error);
+            } else {
+                bot.replyPrivate(message, 'download finish!');
             }
         });
-});
+})
